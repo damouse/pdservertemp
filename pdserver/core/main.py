70aaa9dd3cf858dfb84931459cbe6686474fdcfb
damouse <mickey.barboi@gmail.com>
2015-07-02 18:43:27 -0500

    initial file structure

---
+++ pdserver/core/main.py
@@ -0,0 +1,13 @@
+'''
+Paradrop server v2.
+
+Entry point into the server. Imports protocols/resources from toolsapi and pdapi
+and starts the server here. All database interaction is handled in the db package
+'''
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_general.py
@@ -0,0 +1,10 @@
+
+def testBuild():
+    ''' Tests build successfully '''
+    assert(1)
+
+
+def testImportServer():
+    ''' Server imports correctly '''
+    import pdserver
+    assert 1
