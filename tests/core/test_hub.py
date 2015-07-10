782ba0721de24a75f9c8dc8839fe3dd06a6cab38
Damouse <mickey.barboi@gmail.com>
2015-07-10 15:49:11 -0500

    hub tests working

--- tests/core/test_hub.py
+++ tests/core/test_hub.py
@@ -6,6 +6,9 @@ from pdserver.core import hub
 from pdserver.db import manager
 from nose import tools
 
+from twisted.internet import defer
+
+
 manager.db = manager.Manager(mode='test')
 
 
@@ -14,14 +17,31 @@ def end(res):
     return res
 
 
+def call(res):
+    return manager.db.users.find({'username': 'damouse'})
+
+
+@defer.inlineCallbacks
 @deferred()
 def testRegisterSuceeds():
     d = hub.api_register('damouse', 'damouse123@gmail.com', '12344567')
-    d.addCallback(manager.db.users.find({'username': 'damouse'}))
+
+    d.addCallback(call)
     d.addCallback(lambda x: tools.eq_(len(x), 1))
     d.addBoth(end)
 
     return d
+
+
+# @deferred()
+# def testRegisterSuceeds():
+#     d = hub.api_register('damouse', 'damouse123@gmail.com', '12344567')
+
+#     d.addCallback(call)
+#     d.addCallback(lambda x: tools.eq_(len(x), 1))
+#     d.addBoth(end)
+
+#     return d
 
 
 # @deferred()
